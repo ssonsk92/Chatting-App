d9412cf5a96d1e6e29e5c6165dc56c13332cdd7f
ssonsk92 <101576095+ssonsk92@users.noreply.github.com>
2022-11-14 23:43:37 +0900

    로그인 버튼 id, pw 길이에 따라 활성화/비활성화

--- QtChattingClient/src/LoginPage.cpp
+++ QtChattingClient/src/LoginPage.cpp
@@ -2,9 +2,14 @@
 
 namespace ChatApp
 {
-	LoginPage::LoginPage(QWidget* parent)
+    LoginPage::LoginPage(QWidget* parent)
         : QWidget(parent)
-	{
+        , m_editID(nullptr)
+        , m_editPW(nullptr)
+        , m_buttonLogin(nullptr)
+        , minIDLength(1)
+        , minPWLength(4)
+    {
         QVBoxLayout* layout = new QVBoxLayout;
         layout->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
         layout->setSpacing(0);
@@ -12,25 +17,58 @@ namespace ChatApp
         QLabel* labelTalkImage = new QLabel(this);
         labelTalkImage->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
         labelTalkImage->setPixmap(QPixmap(":image/talk.png"));
-        QLineEdit* editID = new QLineEdit(this);
-        editID->setFixedHeight(40);
-        QLineEdit* editPW = new QLineEdit(this);
-        editPW->setFixedHeight(40);
-        editPW->setEchoMode(QLineEdit::Password);
-        editPW->setPlaceholderText(tr("PASSWORD"));
-        QPushButton* buttonLogin = new QPushButton(tr("LOGIN"), this);
-        buttonLogin->setFixedHeight(40);
+        m_editID = new QLineEdit(this);
+        m_editID->setFixedHeight(40);
+        connect(m_editID, SIGNAL(textChanged(const QString&)), this, SLOT(slotChangedID(const QString&)));
+        m_editPW = new QLineEdit(this);
+        m_editPW->setFixedHeight(40);
+        m_editPW->setEchoMode(QLineEdit::Password);
+        m_editPW->setPlaceholderText(tr("PASSWORD"));
+        connect(m_editPW, SIGNAL(textChanged(const QString&)), this, SLOT(slotChangedPW(const QString&)));
+        m_buttonLogin = new QPushButton(tr("LOGIN"), this);
+        connect(m_buttonLogin, SIGNAL(clicked()), this, SLOT(slotLoginClicked()));
+        m_buttonLogin->setFixedHeight(40);
+        m_buttonLogin->setEnabled(false);
         layout->addWidget(labelTalkImage);
         layout->addSpacing(25);
-        layout->addWidget(editID);
-        layout->addWidget(editPW);
+        layout->addWidget(m_editID);
+        layout->addWidget(m_editPW);
         layout->addSpacing(5);
-        layout->addWidget(buttonLogin);
+        layout->addWidget(m_buttonLogin);
         setLayout(layout);
-	}
+    }
 
-	LoginPage::~LoginPage()
-	{
+    LoginPage::~LoginPage()
+    {
+        
+    }
 
-	}
+    void LoginPage::slotChangedID(const QString& text)
+    {
+        enableLoginButton();
+    }
+
+    void LoginPage::slotChangedPW(const QString& text)
+    {
+        enableLoginButton();
+    }
+
+    void LoginPage::slotLoginClicked()
+    {
+        QString id = m_editID->text();
+        QString pw = m_editPW->text();
+
+        qDebug() << id << pw;
+    }
+
+    void LoginPage::enableLoginButton()
+    {
+        int lengthID = m_editID->text().length();
+        int lengthPW = m_editPW->text().length();
+
+        if (minIDLength <= lengthID && minPWLength <= lengthPW)
+            m_buttonLogin->setEnabled(true);
+        else
+            m_buttonLogin->setEnabled(false);
+    }
 }
--- QtChattingClient/src/LoginPage.h
+++ QtChattingClient/src/LoginPage.h
@@ -13,6 +13,22 @@ namespace ChatApp
 	public:
 		LoginPage(QWidget* parent);
 		~LoginPage();
+
+	public slots:
+		void slotChangedID(const QString& text);
+		void slotChangedPW(const QString& text);
+		void slotLoginClicked();
+
+	private:
+		void enableLoginButton();
+
+	private:
+		QLineEdit* m_editID;
+		QLineEdit* m_editPW;
+		QPushButton* m_buttonLogin;
+
+		const int minIDLength;
+		const int minPWLength;
 	};
 }
 
